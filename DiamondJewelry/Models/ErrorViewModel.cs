f8ea2622e7ed6f7872e41427f4401f6e39226f67
ChienDC <doancaochien.1994@gmail.com>
2025-02-18 09:23:02 +0700

    Initial commit

---
+++ DiamondJewelry/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DiamondJewelry.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
